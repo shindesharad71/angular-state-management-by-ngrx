dbcc25ea08fbaa3648a9d910ddd9fc6fbfb8be51
Sharad Shinde <shindesharad71@gmail.com>
2019-12-15 12:52:16 +0530

    Added: Cross Icon and Css Modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { StoreModule } from '@ngrx/store';
-import { TodoReducer } from '../app/reducers/todo.reducer';
-import { TodoListItemsComponent } from './todo-list-items/todo-list-items.component';
-import { AddItemComponent } from './add-item/add-item.component';
+import { StoreModule } from "@ngrx/store";
+import { TodoReducer } from "../app/reducers/todo.reducer";
+import { TodoListItemsComponent } from "./todo-list-items/todo-list-items.component";
+import { AddItemComponent } from "./add-item/add-item.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListItemsComponent,
-    AddItemComponent
-  ],
+  declarations: [AppComponent, TodoListItemsComponent, AddItemComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({todoList: TodoReducer})
+    FormsModule,
+    StoreModule.forRoot({ todoList: TodoReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
